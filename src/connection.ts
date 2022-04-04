7ab3b9941604a851a59d2edf07308a2b152aaf5c
Deyvid Holz <deyvidholzt@gmail.com>
2022-04-03 22:56:02 -0300

    ✨ implement seeds

--- src/connection.ts
+++ src/connection.ts
@@ -1,23 +1,6 @@
 import { DataSource } from 'typeorm';
-import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
+import connectionConfig from './configs/connection.config';
 
-const dataSource = new DataSource({
-  type: process.env.DB_TYPE,
-  host: process.env.POSTGRES_HOST,
-  port: +(process.env.POSTGRES_PORT || 5432),
-  username: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DB,
-  synchronize: true,
-  entities: [process.env.TYPEORM_DIR_ENTITIES || 'src/**/*.entity.{js,ts}'],
-  migrations: [
-    process.env.TYPEORM_DIR_MIGRATIONS || 'src/**/*.migration.{js,ts}',
-  ],
-  subscribers: [
-    process.env.TYPEORM_DIR_SUBSCRIBERS || 'src/**/*.subscriber.{js,ts}',
-  ],
-  seeds: [process.env.TYPEORM_DIR_SEEDS || 'src/**/*.seed.{js,ts}'],
-  factories: [process.env.TYPEORM_DIR_FACTORIES || 'src/**/*.factory.{js,ts}'],
-} as PostgresConnectionOptions);
+const dataSource = new DataSource(connectionConfig);
 
 export default dataSource;
